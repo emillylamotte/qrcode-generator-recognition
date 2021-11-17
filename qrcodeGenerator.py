53f0d76f65da0420b3f85eb40fe6a40b6c00a76f
Emilly Lamotte <79487290+emillylamotte@users.noreply.github.com>
2021-11-16 23:39:22 -0300

    Create qrcodeGenerator.py

---
+++ qrcodeGenerator.py
@@ -0,0 +1,16 @@
+#gerando qrcode a partir de string
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data("Lamotte")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("Lamotte.png")
